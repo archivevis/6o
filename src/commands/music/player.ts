fb0fc32d68c2ff20b1f989baf626d03c1e895c5a
reese <61914473+reesypiece@users.noreply.github.com>
2021-01-31 17:41:43 -0500

    debating on moving music to a different repo/bot

--- src/commands/music/player.ts
+++
@@ -1,9 +0,0 @@
-import { Player } from 'discord-music-player';
-import { client } from '../../main';
-
-export const player = new Player(client, {
-  leaveOnEnd: false,
-  leaveOnStop: false,
-  leaveOnEmpty: false,
-  quality: 'high',
-});
